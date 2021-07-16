b153b00271917dfdab51ea98281f192883b61fce
jerry3972 <54852021+jerry3972@users.noreply.github.com>
2021-07-16 23:12:57 +0900

    piece making work

---
+++ laser_chess/Assets/Scripts/ShootLaserBlue.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootLaserBlue : MonoBehaviour
+{
+    public Material material;
+    LaserBeamBlue beam;
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(GameObject.Find("Laser Beam Blue"));
+        beam = new LaserBeamBlue(gameObject.transform.position, gameObject.transform.right, material);
+    }
+}
